0f29895f03a519effa5628c841f976e2ee3a3239
flrp <flrping7@gmail.com>
2025-10-18 17:35:03 -0400

    fixed circular dep

--- src/scripts/transition.gd
+++ src/scripts/transition.gd
@@ -2,11 +2,12 @@
 # Use UI to change the "target" parameter to the desired scene
 
 extends Node2D
-@export var target: PackedScene = null
+@export var target: String = "";
+var targetScene: PackedScene = null;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	targetScene = load(target)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,6 +17,5 @@ func _process(delta: float) -> void:
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print(body)
-	#get_tree().change_scene_to_file(target)
-	var result = get_tree().change_scene_to_packed(target)
+	var result = get_tree().change_scene_to_packed(targetScene)
 	print(result)
