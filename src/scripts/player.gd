5dc57083caf25ec5f0d7d86cf633ed8c1cb9668f
Kira Kaviani <chriswhoprograms@gmail.com>
2025-10-18 23:45:49 -0400

    add has_jumped bool
    should be useful for landing animation + might be useful for mechanics
    that we only want to work after a jump

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -7,11 +7,15 @@ const JUMP_VELOCITY = -400.0
 @onready var animation = $AnimatedSprite2D
 
 var run_transition_counter = 0;
+var has_jumped = false
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
+		has_jumped = true
 		velocity += get_gravity() * delta
+	elif has_jumped:
+		has_jumped = false
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
