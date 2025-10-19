bbc351b12e72741a04df02238a9c6cac0a1dd12f
Kira Kaviani <chriswhoprograms@gmail.com>
2025-10-18 23:31:56 -0400

    Add run and idle animations

--- src/scripts/player.gd
+++ src/scripts/player.gd
@@ -4,6 +4,9 @@ extends CharacterBody2D
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+@onready var animation = $AnimatedSprite2D
+
+var run_transition_counter = 0;
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -17,9 +20,22 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
+	
 	if direction:
+		if velocity.x == 0:
+			run_transition_counter = 40
+			animation.play("run_start")
+			
 		velocity.x = direction * SPEED
+		
+		animation.play("run")
+		
+		if velocity.x < 0:
+			animation.flip_h = true
+		else:
+			animation.flip_h = false
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animation.play("idle")
 
 	move_and_slide()
