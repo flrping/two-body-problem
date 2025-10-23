4cf73bbcdc36a2099cb20a5e7aecdcb3ae6e1aa6
Jaelyn <flrping7@gmail.com>
2025-10-23 14:21:28 -0400

    attempt to add cutscenes

--- src/scripts/transition.gd
+++ src/scripts/transition.gd
@@ -3,12 +3,15 @@
 
 extends Node2D
 @export var target: String = "";
+@export var cutscene: String = "";
+
 var targetScene: PackedScene = null;
+
+signal trigger_switch
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	targetScene = load(target)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -18,4 +21,29 @@ func _process(delta: float) -> void:
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	print(body)
 	#apparently altering the scene tree in a physics callback can cause issues
+	trigger_switch.emit()
+
+func _on_trigger_switch():
+	if cutscene.length() > 0:
+		var scene = Control.new()
+		var player = VideoStreamPlayer.new()
+		var stream = VideoStreamTheora.new()
+		stream.file = cutscene
+		player.stream = stream
+		player.expand = true
+		player.loop = false
+		player.size = get_viewport_rect().size
+		player.position = Vector2.ZERO
+		scene.add_child(player)
+		
+		var old = get_tree().current_scene
+		get_tree().root.add_child(scene)
+		get_tree().current_scene = scene
+		old.queue_free()
+		
+		print("Started playing")
+		player.play()
+		await player.finished
+		print("Finished playing")
+		
 	get_tree().call_deferred("change_scene_to_packed", targetScene)
